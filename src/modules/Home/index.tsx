4138db9b5b365b91e59fed4273cf17c69bfe4ea6
Pedro Antonio David Jorge Gomes Pereira <pedroantonio.dj.pereira@gmail.com>
2024-01-18 09:32:22 -0300

    feat: add animejs library and Starry night home

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,9 +1,109 @@
 import React from "react";
+import anime from "animejs/lib/anime.es.js";
 
-import { Container } from "./styles";
+import { Container, ShootingStars, Sky, Wish } from "./styles";
 
 const Home: React.FC = () => {
-  return <Container>Hello</Container>;
+  const num = 200;
+  const vw = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+  const vh = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  );
+
+  const starryNight = () => {
+    anime({
+      targets: ["#sky .star"],
+      opacity: [
+        {
+          duration: 700,
+          value: "0",
+        },
+        {
+          duration: 700,
+          value: "1",
+        },
+      ],
+      easing: "linear",
+      loop: true,
+      delay: (_, i) => 50 * i,
+    });
+  };
+
+  const shootingStars = () => {
+    anime({
+      targets: ["#shootingstars .wish"],
+      easing: "linear",
+      loop: true,
+      delay: (_, i) => 1000 * i,
+      opacity: [
+        {
+          duration: 700,
+          value: "1",
+        },
+      ],
+      width: [
+        {
+          value: "150px",
+        },
+        {
+          value: "0px",
+        },
+      ],
+      translateX: 350,
+    });
+  };
+
+  const randomRadius = () => {
+    return Math.random() * 0.7 + 0.6;
+  };
+
+  const getRandomX = () => {
+    return Math.floor(Math.random() * Math.floor(vw)).toString();
+  };
+
+  const getRandomY = () => {
+    return Math.floor(Math.random() * Math.floor(vh)).toString();
+  };
+
+  React.useEffect(() => {
+    starryNight();
+    shootingStars();
+  }, []);
+
+  return (
+    <Container>
+      <Sky id="sky">
+        {[...Array(num)].map((_, y) => (
+          <circle
+            cx={getRandomX()}
+            cy={getRandomY()}
+            r={randomRadius()}
+            stroke="none"
+            strokeWidth="0"
+            fill="white"
+            key={y}
+            className="star"
+          />
+        ))}
+      </Sky>
+      <ShootingStars id="shootingstars">
+        {[...Array(60)].map((_, y) => (
+          <Wish
+            key={y}
+            className="wish"
+            style={{
+              left: `${getRandomY()}px`,
+              top: `${getRandomX()}px`,
+            }}
+          />
+        ))}
+      </ShootingStars>
+    </Container>
+  );
 };
 
 export default Home;
--- src/modules/Home/styles.ts
+++ src/modules/Home/styles.ts
@@ -1,3 +1,41 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(to right, #ff47a1 0%, #ff9f4d 100%);
+`;
+
+export const Sky = styled.svg`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+`;
+
+export const ShootingStars = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 150vh;
+  height: 100vw;
+  position: fixed;
+  overflow: hidden;
+  transform: translatex(calc(50vw - 50%)) translatey(calc(50vh - 50%))
+    rotate(120deg);
+`;
+
+export const Wish = styled.div`
+  height: 2px;
+  top: 300px;
+  width: 100px;
+  margin: 0;
+  opacity: 0;
+  padding: 0;
+  background-color: white;
+  position: absolute;
+  background: linear-gradient(-45deg, white, rgba(0, 0, 255, 0));
+  filter: drop-shadow(0 0 6px white);
+  overflow: hidden;
+`;
